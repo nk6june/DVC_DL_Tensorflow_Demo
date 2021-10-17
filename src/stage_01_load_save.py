ae5a9e92e1c89e908819209af2c1f33561b57aa6
naresh <naresh.datascience@gmail.com>
2021-10-17 18:27:24 +0530

    logging created

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -6,6 +6,10 @@ import shutil
 from tqdm import tqdm
 import logging
 
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s:"
+log_dir =  "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,'running_logs.log'), level=logging.INFO, format=logging_str, filemode="a")
 
 def copy_file(source_download_dir,local_data_dir):
     list_of_files = os.listdir(source_download_dir)
@@ -33,4 +37,10 @@ if __name__ == '__main__':
 
     parsed_args = args.parse_args()
 
-    get_data(config_path = parsed_args.config)
+    try:
+        logging.info(">>>>>>>> stage one started")
+        get_data(config_path = parsed_args.config)
+        logging.info("stage one completed! all the data are saved in local >>>>>")
+    except Exception as e:
+        logging.exception(e)
+        raise e 
