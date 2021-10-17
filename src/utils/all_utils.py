8dcacb2d13e077b01af62ef08b75c61c18a07d38
naresh <naresh.datascience@gmail.com>
2021-10-17 18:34:54 +0530

    util file updated

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -7,7 +7,7 @@ import logging
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
-    logging.info("yaml file: {path to file} loaded successfully")
+    logging.info(f"yaml file: {path_to_yaml} loaded successfully")
     return content
 
 ##Creating Directories
