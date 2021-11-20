dab1a4871864f33794f6619d5ddb912ed08883e8
naresh <naresh.datascience@gmail.com>
2021-11-20 16:04:45 +0530

    Stage04 Models and callbacks read back

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -1,5 +1,7 @@
 from src.utils.all_utils import read_yaml, create_directory
 from src.utils.models import load_full_model, get_unique_path_to_save_model
+from src.utils.callbacks import get_callbacks
+# from src.utils.data_management import train_valid_generator
 import argparse
 import os
 import logging
@@ -17,8 +19,18 @@ def train_model(config_path, params_path):
     artifacts = config["artifacts"]
     artifacts_dir = artifacts["ARTIFACTS_DIR"]
 
-    
+    train_model_dir_path = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"])
 
+    create_directory([train_model_dir_path])
+
+    untrained_full_model_path = os.path.join(artifacts_dir, artifacts["BASE_MODEL_DIR"], artifacts["UPDATED_BASE_MODEL_NAME"])
+
+    model = load_full_model(untrained_full_model_path)
+
+    callback_dir_path  = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
+    callbacks = get_callbacks(callback_dir_path)
+
+    
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
@@ -30,7 +42,7 @@ if __name__ == '__main__':
     try:
         logging.info(">>>>> stage four started")
         train_model(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info("stage four completed! training completed and model is saved >>>>>>")
+        logging.info("stage four completed! training completed and model is saved >>>>>\n\n")
     except Exception as e:
         logging.exception(e)
         raise e
--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -18,15 +18,27 @@ def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
 
 
 def create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
-    checkpoint_file_path = os.path.join(checkpoint_dir,"ckpt_model.h5")
+    checkpoint_file_path = os.path.join(checkpoint_dir, "ckpt_model.h5")
     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_file_path,
         save_best_only=True
     )
+
     ckpt_callback_filepath = os.path.join(callbacks_dir, "checkpoint_cb.cb")
     joblib.dump(checkpoint_callback, ckpt_callback_filepath)
     logging.info(f"tensorboard callback is being saved at {ckpt_callback_filepath}")
 
 
+def get_callbacks(callback_dir_path):
 
+    callback_path = [
+        os.path.join(callback_dir_path, bin_file) for bin_file in os.listdir(callback_dir_path) if bin_file.endswith(".cb")
+    ]
 
+    callbacks = [
+        joblib.load(path) for path in callback_path
+    ]
+
+    logging.info(f"saved callbacks are loaded from {callback_dir_path}")
+
+    return callbacks
